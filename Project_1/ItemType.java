fd768b0e488839d59daddc6f2bfab28af7789e08
Igor Gonçalves <124288137+Ig0rGon@users.noreply.github.com>
2025-01-22 15:52:53 +0000

    Finished ItemType.java and NodeType.java

--- Project_1/ItemType.java
+++ Project_1/ItemType.java
@@ -1,5 +1,18 @@
 package Project_1;
 
 public class ItemType {
-    
+
+    private int value;
+
+    public int compareTo(ItemType item) {
+        return Integer.compare(this.value, item.value);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void initialize(int num) {
+        this.value = num;
+    }
 }
--- Project_1/NodeType.java
+++ Project_1/NodeType.java
@@ -2,4 +2,11 @@ package Project_1;
 
 public class NodeType {
     
+    public ItemType info;
+    public NodeType next;
+
+    public NodeType(ItemType info) {
+        this.info = info;
+        this.next = null;
+    }
 }
