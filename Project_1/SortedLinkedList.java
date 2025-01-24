366c305af84fd24c81f316d0b106495b946475ac
Siesmo <168371162+Siesmo@users.noreply.github.com>
2025-01-24 16:56:06 -0500

    Added methods for SortedLinkedList

--- Project_1/SortedLinkedList.java
+++ Project_1/SortedLinkedList.java
@@ -1,5 +1,47 @@
 package Project_1;
 
 public class SortedLinkedList {
-    
+    private NodeType head;
+    private NodeType currentPos;
+
+    public SortedLinkedList() {
+        this.head = null;
+        this.currentPos = null;
+    }
+
+    public int getLength() {
+        
+    }
+
+    public void insertItem(ItemType item) {
+
+    }
+
+    public void deleteItem(ItemType item) {
+
+    }
+
+    public int serachItem(ItemType item) {
+
+    }
+
+    public ItemType getNextItem() {
+
+    }
+
+    public void resetList() {
+
+    }
+
+    public mergeList() {
+
+    }
+
+    public deleteAlternateNodes() {
+
+    }
+
+    public intersection() {
+
+    }
 }
