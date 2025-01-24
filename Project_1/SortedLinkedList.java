a452041c0e9a303a62966f1d437eb5b5c30a1943
Siesmo <168371162+Siesmo@users.noreply.github.com>
2025-01-24 17:34:49 -0500

    Updated Sorted

--- Project_1/SortedLinkedList.java
+++ Project_1/SortedLinkedList.java
@@ -10,11 +10,29 @@ public class SortedLinkedList {
     }
 
     public int getLength() {
-
+        int size = 0;
+        if(head == null) {
+            return size;
+        }
+        else {
+            while(currentPos != null) {
+                if(Integer.currentPos.info.getValue() != null) {
+                    size++;
+                }
+                currentPos = currentPos.next;
+            }
+            return size;
+        }
     }
 
     public void insertItem(ItemType item) {
-
+        // If the list is empty
+        if(this.getLength() == 0) {
+            this.head.info = 
+        } else {
+            // If the item to be inserted is new
+            
+        }
     }
 
     public void deleteItem(ItemType item) {
